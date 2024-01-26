b581e0a46c6ae0adf0911a42d0a84d98210688f8
Man0212 <117308273+TiavinaMalalaniaina@users.noreply.github.com>
2024-01-26 22:00:11 +0300

    Update URL.js

--- src/others/URL.js
+++ src/others/URL.js
@@ -1,5 +1,6 @@
 export const BACKEND_URL = "https://varotravam-bg2xwzuf.b4a.run";
-
+ 
 // https://varotravam-bg2xwzuf.b4a.run
 // https://varotravam-backend-production.up.railway.app
 // http://localhost:8080
+//
